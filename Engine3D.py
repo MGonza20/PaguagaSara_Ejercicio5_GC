a25c5af1a21000bb0af9c791dcc7e23035e018b6
MGonza20 <sara.paguaga@gmail.com>
2022-08-15 23:22:19 -0600

    Uploading files

---
+++ Engine3D.py
@@ -0,0 +1,75 @@
+from gl import Renderer, color, V3, V2
+from texture import Texture
+from shaders import flat, unlit, gourad, toon, glow, textureBlend
+
+
+
+#Medium Shot
+def mediumShot():
+    rend = Renderer(960, 540)
+    rend.glViewMatrix(translate=V3(1, 0, 0), rotate=V3(0,0,0))
+    rend.active_texture = Texture("models/model.bmp")
+    rend.active_shader = gourad
+    rend.glLoadModel("models/model.obj",
+                    translate = V3(0,0,-10),
+                    scale = V3(3,3,3),
+                    rotate = V3(0,0,0))
+    rend.glFinish("outputs/mediumShot.bmp")
+
+#High Angle
+def highAngle():
+    rend = Renderer(960, 540)
+    rend.glViewMatrix(translate=V3(0, 5, 0), rotate=V3(-15,0,0))
+    rend.active_texture = Texture("models/model.bmp")
+    rend.active_shader = gourad
+    rend.glLoadModel("models/model.obj",
+                    translate = V3(0,0,-10),
+                    scale = V3(3,3,3),
+                    rotate = V3(0,0,0))
+    rend.glFinish("outputs/highAngle.bmp")
+
+
+#Low Angle
+def lowAngle():
+    rend = Renderer(960, 540)
+    rend.glViewMatrix(translate=V3(1, -2.2, 0), rotate=V3(0,0,0))
+    rend.active_texture = Texture("models/model.bmp")
+    rend.active_shader = gourad
+    rend.glLoadModel("models/model.obj",
+                    translate = V3(0,0,-10),
+                    scale = V3(3,3,3),
+                    rotate = V3(0,0,0))
+    rend.glFinish("outputs/lowAngle.bmp")
+
+
+#Dutch Angle
+def dutchAngle():
+    rend = Renderer(960, 540)
+    rend.glViewMatrix(translate=V3(1, 0, 0), rotate=V3(0,0,-10))
+    rend.active_texture = Texture("models/model.bmp")
+
+    rend.active_shader = gourad
+    rend.glLoadModel("models/model.obj",
+                    translate = V3(0,0,-10),
+                    scale = V3(3,3,3),
+                    rotate = V3(0,35,0))
+    rend.glFinish("outputs/dutchAngle.bmp")
+
+
+def menu():
+    print('\nEjercicio 5 - Graficas\n\nOpciones:\n1. Medium Shot \
+           \n2. High Angle\n3. Low Angle\n4. Dutch Angle\n5. Salir\n')
+
+menu()
+option = int(input('Elija un angulo para el photoshoot: '))
+while option != 5:
+    if option == 1: mediumShot()
+    elif option == 2: highAngle()
+    elif option == 3: lowAngle()
+    elif option == 4: dutchAngle()
+    else: print('\nOpcion invalida\n')
+
+    menu()
+    option = int(input('Elija un angulo para el photoshoot: '))
+
+print('Se ha acabado el photoshoot! Chequea los resultados en la carpeta de outputs.\n')
