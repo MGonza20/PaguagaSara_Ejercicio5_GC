baaf8ff2f3988c3a3851627066ed080873f6cf77
MGonza20 <sara.paguaga@gmail.com>
2022-08-15 23:45:42 -0600

    Adding model

--- Engine3D.py
+++ Engine3D.py
@@ -55,21 +55,34 @@ def dutchAngle():
                     rotate = V3(0,35,0))
     rend.glFinish("outputs/dutchAngle.bmp")
 
+def glowModel():
+    rend = Renderer(960, 540)
+    rend.glViewMatrix(translate=V3(0, 0, 0), rotate=V3(0,0,0))
+    rend.active_texture = Texture("models/model.bmp")
+
+    rend.active_shader = glow
+    rend.glLoadModel("models/model.obj",
+                     translate = V3(0, 0, -8),
+                     scale = V3(3,3,3),
+                     rotate = V3(0,0,0))
+    rend.glFinish("outputs/glowModel.bmp")
+
 
 def menu():
     print('\nEjercicio 5 - Graficas\n\nOpciones:\n1. Medium Shot \
-           \n2. High Angle\n3. Low Angle\n4. Dutch Angle\n5. Salir\n')
+           \n2. High Angle\n3. Low Angle\n4. Dutch Angle\n5. Renderizar modelo glow (Generado en clase)\n6. Salir\n')
 
 menu()
 option = int(input('Elija un angulo para el photoshoot: '))
-while option != 5:
+while option != 6:
     if option == 1: mediumShot()
     elif option == 2: highAngle()
     elif option == 3: lowAngle()
     elif option == 4: dutchAngle()
+    elif option == 5: glowModel()
     else: print('\nOpcion invalida\n')
 
     menu()
     option = int(input('Elija un angulo para el photoshoot: '))
 
-print('Se ha acabado el photoshoot! Chequea los resultados en la carpeta de outputs.\n')
+print('Se ha acabado el photoshoot! Chequee los resultados en la carpeta de outputs.\n')
